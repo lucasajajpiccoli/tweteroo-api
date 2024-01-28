8e9bd55e305df1eba6dc5f7efa4ddd17b31d0271
Lucas Ajaj Piccoli <lucasajaj@gmail.com>
2024-01-28 13:31:16 -0300

    Data models and repositories created

---
+++ src/main/java/com/tweteroo/api/models/TweetModel.java
@@ -0,0 +1,28 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tweets")
+public class TweetModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(length = 280, nullable = false)
+    private String text;
+    
+}
